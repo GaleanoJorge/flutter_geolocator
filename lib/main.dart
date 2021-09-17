b5bf570352000ceb87516ab4799fc07a2433fcae
Jorge Galeano <gidesarrolladorapp1@hlips.com.co>
2021-09-17 07:54:43 -0500

    marcadores y distancias

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
-
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:maps_toolkit/maps_toolkit.dart' as mp;
 
 void main() => runApp(MyApp());
 
@@ -26,84 +28,23 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(4.65, -74.1);
+  final LatLng _center = const LatLng(4.6432365053459295, -74.05374202877283);
+  final mp.LatLng _from = mp.LatLng(4.6432365053459295, -74.05374202877283);
 
   late Position currentPosition;
 
   var geolocator = Geolocator();
   final Map<String, Marker> _markers = {};
 
-  int _selectedIndex = 0;
-
-  // ignore: prefer_final_fields
-  List<Widget> _widgetOptions = <Widget>[
-    const Text(
-      '',
-      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-    ),
-    Container(
-      color: Colors.black54,
-      child: AlertDialog(
-        title: const Text('Alerta'),
-        content: Text('Alerta de uso de Flutter en Agenda'),
-        actions: [
-          FlatButton(onPressed: () {}, child: const Text('OK')),
-          FlatButton(onPressed: () {}, child: const Text('CERRAR')),
-        ],
-      ),
-    ),
-    Image.network(
-        'https://ii.ct-stc.com/1/logos/empresas/2018/07/09/healthlife-sas-C338ED616469628A151449159thumbnail.png'),
-    const Text(
-      'Perfil',
-      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        key: Key('Hola'),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              drawerBoton('message', Icons.message),
-              drawerBoton('primera', Icons.price_change),
-              drawerBoton('segunda', Icons.pedal_bike_outlined),
-              drawerBoton('perfil', Icons.person),
-              drawerBoton('calendar', Icons.calendar_today),
-              drawerBoton('info', Icons.info),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        key: const Key('BND'),
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        currentIndex: _selectedIndex,
-        items: <BottomNavigationBarItem>[
-          bottonItem(Icons.home, 'Inicio'),
-          bottonItem(Icons.calendar_today, 'Agenda'),
-          bottonItem(Icons.health_and_safety_outlined, 'Servicios'),
-          bottonItem(Icons.settings, 'Perfil'),
-        ],
-        onTap: (val) {
-          setState(() {
-            _selectedIndex = val;
-          });
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.message),
-        onPressed: () {},
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-      // Api de maps
       body: Stack(
         children: [
           GoogleMap(
+              // onCameraMove: (CameraPosition val) {
+              //   print(val);
+              // },
               myLocationEnabled: true,
               zoomGesturesEnabled: true,
               zoomControlsEnabled: true,
@@ -111,10 +52,14 @@ class _MyHomePageState extends State<MyHomePage> {
               onMapCreated: _onMapCreated,
               markers: _markers.values.toSet(),
               initialCameraPosition:
-                  CameraPosition(target: _center, zoom: 15.0)),
-          Center(
-            child: _widgetOptions.elementAt(_selectedIndex),
-          )
+                  CameraPosition(target: _center, zoom: 16.0)),
+          // Center(
+          //   child: Container(
+          //     width: 5,
+          //     height: 5,
+          //     color: Colors.red,
+          //   ),
+          // )
         ],
       ),
     );
@@ -127,47 +72,82 @@ class _MyHomePageState extends State<MyHomePage> {
         desiredAccuracy: LocationAccuracy.best);
     currentPosition = position;
 
-    LatLng latLngPosition = LatLng(position.latitude, position.longitude);
+    // LatLng latLngPosition = LatLng(position.latitude, position.longitude);
 
-    CameraPosition cameraPosition =
-        CameraPosition(target: latLngPosition, zoom: 16.0);
-    mapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
-    setState(() {
-      _markers['Mi Ubicación'] = Marker(
-        markerId: const MarkerId('Mi Ubicación'),
-        position: latLngPosition,
-        infoWindow:
-            const InfoWindow(title: 'Mi Ubicación', snippet: 'Jorge Galeano'),
+    // CameraPosition cameraPosition =
+    //     CameraPosition(target: latLngPosition, zoom: 14.0);
+    // mapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+    List<LatLng> points = [];
+    points.add(const LatLng(4.645786261906281, -74.05308354645967));
+    points.add(const LatLng(4.644555495899668, -74.05268523842096));
+    points.add(const LatLng(4.643342773234623, -74.05390832573175));
+    points.add(const LatLng(4.64343968418833, -74.05554950237274));
+    points.add(const LatLng(4.645206468793103, -74.05086971819401));
+
+    List<double> distances = [];
+
+    for (int i = 0; i < points.length; i++) {
+      mp.LatLng point = mp.LatLng(points[i].latitude, points[i].longitude);
+      double distance = (mp.SphericalUtil.computeDistanceBetween(
+              _from, point) /
+          1.0);
+      print("La distancia desde mi ubicación es: " + distance.toString());
+      distances.add(distance);
+    }
+
+    distances.sort();
+    for(double i in distances){
+      print(i);
+    }
+    print('el más cercano es: ' + distances[0].toString());
+
+    final Uint8List markerIcon = await getBytesFromCanvas(200, 100);
+
+    for (int i = 0; i < points.length; i++) {
+      String destiny = 'Destino ' + (i).toString();
+      _markers[destiny] = Marker(
+        icon: BitmapDescriptor.fromBytes(markerIcon),
+        markerId: MarkerId(destiny + 'id visible?' + (i*2).toString()),
+        position: LatLng(points[i].latitude, points[i].longitude),
+        infoWindow: InfoWindow(title: destiny, snippet: destiny),
       );
+    }
+    
+
+    setState(() {
+      _markers['Mi Ubicación'] = const Marker(
+        markerId: MarkerId('Mi Ubicación'),
+        position: LatLng(4.6432365053459295, -74.05374202877283),
+        infoWindow: InfoWindow(title: 'Mi Ubicación', snippet: 'Jorge Galeano'),
+      );
+
     });
   }
 
-  Padding drawerBoton(String message, IconData icon) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      // ignore: deprecated_member_use
-      child: FlatButton(
-        onPressed: () {},
-        child: Row(
-          children: <Widget>[
-            Icon(icon),
-            const SizedBox(
-              width: 7.0,
-            ),
-            Text(
-              message,
-              style: const TextStyle(fontSize: 15.0),
-            )
-          ],
-        ),
+  Future<Uint8List> getBytesFromCanvas(int width, int height) async {
+  final ui.PictureRecorder pictureRecorder = ui.PictureRecorder();
+  final Canvas canvas = Canvas(pictureRecorder);
+  final Paint paint = Paint()..color = Colors.blue;
+  final Radius radius = Radius.circular(20.0);
+  canvas.drawRRect(
+      RRect.fromRectAndCorners(
+        Rect.fromLTWH(0.0, 0.0, width.toDouble(), height.toDouble()),
+        topLeft: radius,
+        topRight: radius,
+        bottomLeft: radius,
+        bottomRight: radius,
       ),
-    );
-  }
-
-  BottomNavigationBarItem bottonItem(IconData icon, String message) =>
-      BottomNavigationBarItem(
-        icon: Icon(icon),
-        label: message,
-        backgroundColor: Colors.white,
-      );
+      paint);
+  TextPainter painter = TextPainter(textDirection: TextDirection.ltr);
+  painter.text = TextSpan(
+    text: 'Doctor',
+    style: TextStyle(fontSize: 25.0, color: Colors.white),
+  );
+  painter.layout();
+  painter.paint(canvas, Offset((width * 0.5) - painter.width * 0.5, (height * 0.5) - painter.height * 0.5));
+  final img = await pictureRecorder.endRecording().toImage(width, height);
+  final data = await img.toByteData(format: ui.ImageByteFormat.png);
+  return data!.buffer.asUint8List();
+}
 }
